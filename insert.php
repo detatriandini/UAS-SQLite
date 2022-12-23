5520035bd2887f52092b628e08a837c1101336e1
farrasrw <94120141+farrasrw@users.noreply.github.com>
2022-12-23 22:05:42 +0700

    changeinsert

--- insert.php
+++ insert.php
@@ -7,7 +7,6 @@ $email = $_POST['uemail'];
 $query = "INSERT INTO user_table(user_name, user_email) VALUES( '$name', '$email');";
 if ($db->exec($query)) {
     header("Location:index.php");
-    //    echo "Successfully Insert Records";
 } else {
     echo "error with the query";
 }
